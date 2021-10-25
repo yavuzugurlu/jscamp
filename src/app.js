bace5a72e89abaacc97e1f5880e57de61ddcd6dc
Yavuz UĞURLU <yavuz.ugurlu06@outlook.com>
2021-10-25 18:36:27 +0300

    25 Ekim 2021

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("Merhaba Kodlama.io")
+
+
+let dolarDun = 9.20
+let dolarBugun = 9.30 
+dolarDun = "9.20"
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 const variable değiştirilmez
+
+console.log(euroDun)
+
+//array
+//camelCasing
+let konutKredileri = ["Deneme1","Deneme2","Deneme3"] 
+
+console.log("<ul>")
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log("<li>"+ konutKredileri[i]+"</li>")
+    
+}
+console.log("</ul>")
+
+console.log(konutKredileri)
